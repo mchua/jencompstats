417dfe4a3034d0fe3868f0c4ff9e7ad33a04a021
Mel Chua <mel@redhat.com>
2012-05-13 23:44:32 -0400

    fixing typo of 'geschlecht' and adding ausweisnummer attribute

--- Respondent.py
+++ Respondent.py
@@ -1,11 +1,26 @@
 class Respondent(object):
-    """Jeden Respondent Objekt stellt Eine Person in die 2011-2012 Personalpronomen Daten. Ich habe mein Fachvokabular aus http://ada.rg16.asn-wien.ac.at/~python/how2think/ genommen. Ich weiss nicht wie man ä ö ü ß schreibt."""
-    def __init__(self, geschlecht=0, alter = 0, aktueller_wohnort = 0, jahre_an_ort = 0, davor_gewohnt = 0, lange_davor_gewohnt = 0, familienstand = 0, muttersprache = 0, polyglotter = 0, schulabschluss = 0, q44 = 0):
+    """Jeden Respondent Objekt stellt Eine Person in die 2011-2012 Personalpronomen Daten. Ich habe mein Fachvokabular aus http://ada.rg16.asn-wien.ac.at/~python/how2think/ genommen. Ich weiss nicht wie man Umlaut oder Eszett schreibt."""
+    def __init__(self,
+		 ausweisnummer = 0,
+		 geschlecht=0,
+		 alter = 0,
+		 aktueller_wohnort = 0,
+		 jahre_an_ort = 0,
+		 davor_gewohnt = 0,
+		 lange_davor_gewohnt = 0,
+		 familienstand = 0,
+		 muttersprache = 0,
+		 polyglotter = 0,
+		 schulabschluss = 0,
+		 q44 = 0):
 	"""Ein neues Respondent Objekt machen."""
         """BEACHT: Ich weiss, dass man musst Substantive grossschrieben, aber auf Python man grossschriebt nicht Attribute, sondern Objekte."""
 	"""BEACHT: Wir konnen 0 == 'keine Antwort' benutzen, weil Qualtrics mit 1 Zahlen beginnt. Praktisch."""
+	# Ausweisnummer
+	"""Wir machen jede Respondent Objekt eine Ausweisnummer."""
+	self.ausweisnummer = ausweisnummer
 	# Q1
-        self.geschlect = geschlecht
+        self.geschlecht = geschlecht
 		# 0 bedeutet keine Antwort
 		# 1 bedeutet Mann
 		# 2 beduetet Frau
@@ -23,14 +38,14 @@ class Respondent(object):
 		# 9 bedeutet 60-64 Jahre alt
 		# 10 bedeutet 65-69 Jahre alt
 		# 11 bedeutet 70+ Jahre alt
-		"""FRAGEN: mochtest du die aktuelle Nummern benutzen? Z.B. nicht '8', sondern '55'?"""
+	"""FRAGEN: mochtest du die aktuelle Nummern benutzen? Z.B. nicht '8', sondern '55'?"""
 	# Q3
 	self.aktueller_wohnort = aktueller_wohnort
 		# 0 bedeutet keine Antwort
 		# Anders dies Attribute sind eine String
 	# Q4
 	self.jahre_an_ort = jahre_an_ort # Q4
-		"""Jahre an diesem Ort"""
+	"""Jahre an diesem Ort"""
 		# 0 bedeutet keine Antwort
 		# 1 bedeutet 0-5 Jahre
 		# 2 bedeutet 6-10 Jahre
@@ -39,20 +54,20 @@ class Respondent(object):
 		# 5 bedeutet 21+ Jahre
 	# Q4a
 	self.davor_gewohnt = davor_gewohnt # Q4a
-		"""Wo haben Sie davor gewohnt?"""
+	"""Wo haben Sie davor gewohnt?"""
 		# 0 bedeutet keine Antwort
 		# Anders dies Attribute sind eine String
-		"""BEACHT: Keine Personen geantwortet (2011-2012 Daten)."""
+	"""BEACHT: Keine Personen geantwortet (2011-2012 Daten)."""
 	# Q4b
 	self.lange_davor_gewohnt = lange_davor_gewohnt # Q4b
-		"""Wie lange haben Sie dort gewohnt?"""
+	"""Wie lange haben Sie dort gewohnt?"""
 		# 0 bedeutet keine Antwort
 		# 1 bedeutet 0-5 Jahre
 		# 2 bedeutet 6-10 Jahre
 		# 3 bedeutet 11-15 Jahre
 		# 4 bedeutet 16-20 Jahre
 		# 5 bedeutet 21+ Jahre
-		"""BEACHT: Keine Personen geantwortet (2011-2012 Daten)."""
+	"""BEACHT: Keine Personen geantwortet (2011-2012 Daten)."""
 	# Q5
 	self.familienstand = familienstand
 		# 0 bedeutet keine Antwort
@@ -65,7 +80,7 @@ class Respondent(object):
 	self.muttersprache = muttersprache
 		# 0 bedeutet keine Antwort
 		# Anders dies Attribute sind eine String
-		"""FRAGEN: Einige Personen 'Englisch' sagen, und andere 'English' oder 'english' usw. sagen. Mochten wir dieses Antworten gleich benutzen? Wie kann man hier Python schreibt, um zu dass machen?"""
+	"""FRAGEN: Einige Personen 'Englisch' sagen, und andere 'English' oder 'english' usw. sagen. Mochten wir dieses Antworten gleich benutzen? Wie kann man hier Python schreibt, um zu dass machen?"""
 	# Q7
 	"""Haben Sie mehr als eine Muttersprache?"""
 	if polyglotter == 1: 
