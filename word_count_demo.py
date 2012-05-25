afbb1d93fbd9d08f35e03d41d8b1ed0dff8c63f1
Mel Chua <mel@redhat.com>
2012-05-24 22:13:11 -0400

    adding word count demo code

---
+++ word_count_demo.py
@@ -0,0 +1,20 @@
+# allow us to use the defaultdict function.
+# what defaultdict does isn't really important for our purposes now.
+from collections import defaultdict
+
+# open the text file we want to read from
+inputfile = open('Social Meanings of Sie.txt', 'r')
+
+# read the entire file into a string
+words = inputfile.read()
+
+# count the words...
+# this code is sort of magic, until you learn dictionaries.
+# once you do, you'll be able to understand it.
+wordcount = defaultdict(int)
+for word in words.split():
+    wordcount[word] += 1
+
+# and print them.
+# this code is magic; do not question it.
+print sorted(wordcount.items(), key=lambda item: item[1])
